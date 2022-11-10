964ac91768a2d3cfdf15e1fe897eb64d503dcaa3
Tom Horton <thomasphorton@gmail.com>
2022-11-10 10:28:06 -0500

    Adds lapdata listener

--- src/VideoComponent.js
+++ src/VideoComponent.js
@@ -8,7 +8,11 @@ class Dashboard extends React.Component {
         super(props);
 
         this.client = new Ably.Realtime({ key: process.env.REACT_APP_ABLY_KEY })
-        this.channel = this.client.channels.get('f1-telemetry', {
+        this.telemetryChannel = this.client.channels.get('f1-telemetry', {
+            params: { rewind: '1' }
+        });
+
+        this.lapDataChannel = this.client.channels.get('f1-lapData', {
             params: { rewind: '1' }
         });
 
@@ -18,40 +22,59 @@ class Dashboard extends React.Component {
             gear: 0,
             throttlePercentage: 0,
             brakePercentage: 0,
-            steering: 0
+            steering: 0,
+            lapData: []
         };
 
-        this.channel.subscribe('playerCarTelemetry', (msg) => {
+        this.telemetryChannel.subscribe('playerCarTelemetry', (msg) => {
             console.log(msg.data);
-            let speed = msg.data.m_speed;
 
-            if  (speed) {
-                this.setState({speed})
+            if (msg.data.m_speed) {
+                let speed = msg.data.m_speed;
+
+                if  (speed) {
+                    this.setState({speed})
+                }
+    
+                let engineRPM = msg.data.m_engineRPM;
+    
+                if (engineRPM) {
+                    this.setState({engineRPM})
+                }
+                
+                let gear = msg.data.m_gear;
+                if (gear) {
+                    this.setState({gear})
+                }
+    
+                let throttle = msg.data.m_throttle;
+                let throttlePercentage = 0;
+                if (typeof throttle) {
+                    throttlePercentage = (throttle * 100).toFixed(0);
+                }
+                this.setState({throttlePercentage})
+    
+                let brake = msg.data.m_brake;
+                let brakePercentage = 0;
+                if (typeof brake) {
+                    brakePercentage = (brake * 100).toFixed(0);
+                }
+                this.setState({brakePercentage})
+    
+                let steering = 0;
+                console.log(typeof msg.data.m_steer)
+                if (typeof msg.data.m_steer) {
+                    steering = msg.data.m_steer.toFixed(3);
+                }
+                
+                this.setState({steering})
             }
+        });
 
-            let engineRPM = msg.data.m_engineRPM;
-
-            if (engineRPM) {
-                this.setState({engineRPM})
-            }
-            
-            let gear = msg.data.m_gear;
-            if (gear) {
-                this.setState({gear})
-            }
-
-            let throttle = msg.data.m_throttle;
-            let throttlePercentage = (throttle * 100).toFixed(0)
-            this.setState({throttlePercentage})
-
-            let brake = msg.data.m_brake;
-            let brakePercentage = (brake * 100).toFixed(0)
-            this.setState({brakePercentage})
-
-            let steering = msg.data.m_steer.toFixed(3);
-            
-            this.setState({steering})
-
+        this.lapDataChannel.subscribe('lapData', (msg) => {
+            console.log(msg);
+            let lapData = msg.data;
+            this.setState({lapData});
         });
     }
     render() {
@@ -104,6 +127,15 @@ class Dashboard extends React.Component {
                         />
                     </div>
                 </div>
+                <h2>Lap Times</h2>
+                <ul>
+                {
+                    this.state.lapData.map(lapTime => {
+
+                        return (<li>{(lapTime/1000).toFixed(3)}</li>)
+                    })
+                }
+                </ul>
             </div>
         )
     }
